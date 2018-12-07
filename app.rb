0be15eef523c58c0a5bb20527d76bf44ad08b82b
Ben Kirby <benkrby@gmail.com>
2018-12-07 13:11:14 -0800

    Can now add volunteers via web page.

--- app.rb
+++ app.rb
@@ -7,6 +7,22 @@ require('pry')
 DB = PG.connect({:dbname => 'volunteer_tracker'})
 
 get '/' do
-
   erb(:index)
+end
+
+get '/add_volunteer' do
+    @volunteers = Volunteer.all
+    erb(:add_volunteer)
+end
+
+post '/add_volunteer' do
+  name = params.fetch('name')
+  new_volunteer = Volunteer.new({:id => nil, :name => name, :project_id => 0})
+  new_volunteer.save
+  @volunteers = Volunteer.all
+  erb(:add_volunteer)
+end
+
+get '/view_volunteer' do
+  "Hello World"
 end
